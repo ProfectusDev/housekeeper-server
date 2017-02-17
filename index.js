a266e78f6318d2589e50cbea57dc792f87efcb7a
John Fiorentino <jfiorentino3@gatech.edu>
2017-02-16 19:44:55 -0500

    General Updates
    @John_Fio

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ var connection = mysql.createConnection({
   database : 'Housekeeper'
 });
 
-// Remove user from the database
-app.delete('/deleteUser', function (req, res) {
-  //code
-})
 
 // Create new user and add to the database
 app.get('/createUser', function(req, res) {
@@ -64,6 +60,70 @@ app.get('/createSession', function(req, res) {
     }
   });
 })
+
+// add a House object to the user profile
+app.get('/addHouse', function(req, res) {
+  // User Information
+  var user_id = req.query.user_id;
+
+  // House Information
+  var address = req.query.address;
+
+  // var location = req.query.location;
+  var image = req.query.image;
+
+  // var categories = req.query.categories;
+
+  // execute query
+  var query_str = "INSERT INTO Houses (user_id, address) VALUES('" + user_id + "', '" + address + "');"
+  connection.query(query_str, function (error, results, fields) {
+    if (error) {
+      res.status(500).send('Error: ' + error.code);
+      console.log('Error: ' + error.code);
+    } else {
+      res.send('Successfully Added House');
+      console.log('Added House with address: ' + address);
+    }
+  });
+})
+
+// Remove house from the user's house-list
+app.delete('/deleteHouse', function(req, res) {
+  var user_id = req.query.user_id;
+  var house_id = req.query.user_id;
+  var query_str = "DELETE FROM Houses WHERE (house_id = '" + house_id + "');"
+  connection.query(query_str, function(error, results, fields){
+    if (error) {
+      res.status(500).send('Error: ' + error.code);
+      console.log('Error: ' + error.code);
+    } else {
+      res.send('Successfully Removed House');
+      console.console.log('Removed House from List: ' + house_id);
+    }
+  });
+})
+
+// Logout a user and mark their session token as 'inactive'
+app.delete('/logout', function(req,res) {
+  //code
+})
+
+// Remove user from the database
+app.delete('/deleteUser', function (req, res) {
+  //code
+  var user_id = req.query.user_id;
+  var query_str = "DELETE FROM Users WHERE (user_id = '" + user_id + "');"
+  connection.query(query_str, function(error, results, fields) {
+    if (error) {
+      res.status(500).send('Error: ' + error.code);
+      console.log('Error: ' + error.code);
+    } else {
+      res.send('Successfully Removed User');
+      console.log('Removed User from the HouseKeeper Database: ' + user_id);
+    }
+  });
+})
+
 
 connection.connect();
 app.listen(8081, function() {
