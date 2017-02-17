f10e53e62270013cf2716124bfa2c674f2a65ea1
Arjun Chib <arjunpatrickchib@gmail.com>
2017-02-16 19:40:29 -0500

    tokens and post methods

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
 // Contact them for Questions
 
 var express = require('express');
+var mysql      = require('mysql');
+var jwt = require('jsonwebtoken');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -14,15 +17,15 @@ var connection = mysql.createConnection({
   database : 'Housekeeper'
 });
 
-// Remove user from the database
-app.delete('/deleteUser', function (req, res) {
-  //code
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var secret = process.env.JWT_SECRET;
 
 // Create new user and add to the database
-app.get('/createUser', function(req, res) {
-  var email = req.query.email;
-  var password = req.query.password;
+app.post('/createUser', function(req, res) {
+  var email = req.body['email'];
+  var password = req.body.['password'];
   var query_str = "INSERT INTO Users (email, password) VALUES ('" + email + "', '" + password + "');"
   connection.query(query_str, function (error, results, fields) {
     if (error) {
@@ -41,10 +44,10 @@ app.get('/createUser', function(req, res) {
 })
 
 // Login a user and create a session token
-app.get('/createSession', function(req, res) {
-  var email = req.query.email;
-  var password = req.query.password;
-  var query_str = "SELECT password FROM Users WHERE (email = '" + email + "');"
+app.post('/createSession', function(req, res) {
+  var email = req.body['email'];
+  var password = req.body['password'];
+  var query_str = "SELECT id, password FROM Users WHERE (email = '" + email + "');"
   connection.query(query_str, function (error, results, fields) {
     if (error) {
       if (error.code === 'ER_DUP_ENTRY') {
@@ -57,13 +60,61 @@ app.get('/createSession', function(req, res) {
     } else if (results.length === 0) {
       res.status(500).send('This email has not been registered.');
     } else if (results[0]['password'] === password) {
-      res.send('Logged in.');
+      var token = jwt.sign({id: results[0]['id']}, secret);
+      res.send(token);
       console.log('Logged in user with email: ' + email);
     } else {
       res.status(500).send('Email and password do not match.');
     }
   });
 })
+
+// add a House object to the user profile
+app.post('/addHouse', function(req, res) {
+  var token = req.body['token'];
+  try {
+    var decoded = jwt.verify(token, secret);
+  } catch(err) {
+    console.log('Invalid token attempted.');
+    return
+  }
+
+  console.log(decoded);
+
+  // User Information
+  // var user_id = req.query.user_id;
+
+  // House Information
+  var address = req.query.address;
+
+  // var location = req.query.location;
+  var image = req.query.image;
+
+  // var categories = req.query.categories;
+
+  // execute query
+  var query_str = "INSERT INTO Houses (hid, address) VALUES('" + user_id + "', '" + address + "');"
+  connection.query(query_str, function (error, results, fields) {
+    if (error) {
+      res.status(500).send('Error: ' + error.code);
+      console.log('Error: ' + error.code);
+    } else {
+      res.send('Successfully Added House!');
+      console.log('Added House with address: ' + address);
+    }
+  });
+})
+
+// Logout a user and mark their session token as 'inactive'
+app.delete('/logout', function(req,res) {
+  //code
+})
+
+// Remove user from the database
+app.delete('/deleteUser', function (req, res) {
+  //code
+})
+
 
 connection.connect();
 app.listen(8081, function() {
